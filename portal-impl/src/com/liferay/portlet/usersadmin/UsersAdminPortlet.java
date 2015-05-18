36db955d86b41a301f13ca5a01d3ebedc460c2fd
Pei-Jung Lan <pei-jung.lan@liferay.com>
2015-05-18 14:00:12 -0700

    LPS-55590 Convert EditUserAction to MVCPortlet actions

--- portal-impl/src/com/liferay/portlet/usersadmin/UsersAdminPortlet.java
+++ portal-impl/src/com/liferay/portlet/usersadmin/UsersAdminPortlet.java
@@ -17,30 +17,52 @@ package com.liferay.portlet.usersadmin;
 import com.liferay.portal.AddressCityException;
 import com.liferay.portal.AddressStreetException;
 import com.liferay.portal.AddressZipException;
+import com.liferay.portal.CompanyMaxUsersException;
+import com.liferay.portal.ContactBirthdayException;
+import com.liferay.portal.ContactNameException;
 import com.liferay.portal.DuplicateOrganizationException;
 import com.liferay.portal.EmailAddressException;
+import com.liferay.portal.GroupFriendlyURLException;
 import com.liferay.portal.NoSuchCountryException;
 import com.liferay.portal.NoSuchListTypeException;
 import com.liferay.portal.NoSuchOrgLaborException;
 import com.liferay.portal.NoSuchOrganizationException;
 import com.liferay.portal.NoSuchRegionException;
+import com.liferay.portal.NoSuchUserException;
 import com.liferay.portal.OrganizationNameException;
 import com.liferay.portal.OrganizationParentException;
 import com.liferay.portal.PhoneNumberException;
 import com.liferay.portal.RequiredOrganizationException;
+import com.liferay.portal.RequiredUserException;
+import com.liferay.portal.UserEmailAddressException;
+import com.liferay.portal.UserFieldException;
+import com.liferay.portal.UserIdException;
+import com.liferay.portal.UserPasswordException;
+import com.liferay.portal.UserReminderQueryException;
+import com.liferay.portal.UserScreenNameException;
+import com.liferay.portal.UserSmsException;
 import com.liferay.portal.WebsiteURLException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Organization;
+import com.liferay.portal.model.User;
 import com.liferay.portal.security.auth.PrincipalException;
 import com.liferay.portal.security.membershippolicy.MembershipPolicyException;
 import com.liferay.portal.service.OrgLaborServiceUtil;
 import com.liferay.portal.service.OrganizationLocalServiceUtil;
+import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.service.UserGroupServiceUtil;
+import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.service.UserServiceUtil;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
 
 import java.io.IOException;
 
@@ -55,6 +77,42 @@ import javax.portlet.RenderResponse;
  */
 public class UsersAdminPortlet extends MVCPortlet {
 
+	public void deactivateUsers(
+		ActionRequest actionRequest,
+		ActionResponse actionResponse) throws Exception {
+
+		try {
+			long[] deleteUserIds = StringUtil.split(
+				ParamUtil.getString(actionRequest, "deleteUserIds"), 0L);
+
+			for (long deleteUserId : deleteUserIds) {
+				int status = WorkflowConstants.STATUS_INACTIVE;
+
+				UserServiceUtil.updateStatus(
+					deleteUserId, status, new ServiceContext());
+			}
+
+			sendEditUserRedirect(actionRequest, actionResponse);
+		}
+		catch (Exception e) {
+			String password1 = actionRequest.getParameter("password1");
+			String password2 = actionRequest.getParameter("password2");
+
+			boolean submittedPassword = false;
+
+			if (e instanceof RequiredUserException ||
+				!Validator.isBlank(password1) ||
+				!Validator.isBlank(password2)) {
+
+				handleEditUserException(
+					actionRequest, actionResponse, submittedPassword);
+			}
+			else {
+				throw e;
+			}
+		}
+	}
+
 	public void deleteOrgLabor(
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
@@ -62,6 +120,158 @@ public class UsersAdminPortlet extends MVCPortlet {
 		long orgLaborId = ParamUtil.getLong(actionRequest, "orgLaborId");
 
 		OrgLaborServiceUtil.deleteOrgLabor(orgLaborId);
+	}
+
+	public void deleteRole(
+		ActionRequest actionRequest,
+		ActionResponse actionResponse) throws Exception {
+
+		try {
+			User user = PortalUtil.getSelectedUser(actionRequest);
+
+			long roleId = ParamUtil.getLong(actionRequest, "roleId");
+
+			UserServiceUtil.deleteRoleUser(roleId, user.getUserId());
+
+			sendEditUserRedirect(actionRequest, actionResponse);
+		}
+		catch (Exception e) {
+			String password1 = actionRequest.getParameter("password1");
+			String password2 = actionRequest.getParameter("password2");
+
+			boolean submittedPassword = false;
+
+			if (e instanceof RequiredUserException ||
+				!Validator.isBlank(password1) ||
+				!Validator.isBlank(password2)) {
+
+				handleEditUserException(
+					actionRequest, actionResponse, submittedPassword);
+			}
+			else {
+				throw e;
+			}
+		}
+	}
+
+	public void deleteUsers(
+		ActionRequest actionRequest,
+		ActionResponse actionResponse) throws Exception {
+
+		try {
+			long[] deleteUserIds = StringUtil.split(
+				ParamUtil.getString(actionRequest, "deleteUserIds"), 0L);
+
+			for (long deleteUserId : deleteUserIds) {
+				UserServiceUtil.deleteUser(deleteUserId);
+			}
+
+			sendEditUserRedirect(actionRequest, actionResponse);
+		}
+		catch (Exception e) {
+			String password1 = actionRequest.getParameter("password1");
+			String password2 = actionRequest.getParameter("password2");
+
+			boolean submittedPassword = false;
+
+			if (e instanceof RequiredUserException ||
+				!Validator.isBlank(password1) ||
+				!Validator.isBlank(password2)) {
+
+				handleEditUserException(
+					actionRequest, actionResponse, submittedPassword);
+			}
+			else {
+				throw e;
+			}
+		}
+	}
+
+	public void restoreUsers(
+		ActionRequest actionRequest,
+		ActionResponse actionResponse) throws Exception {
+
+		try {
+			long[] deleteUserIds = StringUtil.split(
+				ParamUtil.getString(actionRequest, "deleteUserIds"), 0L);
+
+			for (long deleteUserId : deleteUserIds) {
+				int status = WorkflowConstants.STATUS_APPROVED;
+
+				UserServiceUtil.updateStatus(
+					deleteUserId, status, new ServiceContext());
+			}
+
+			sendEditUserRedirect(actionRequest, actionResponse);
+		}
+		catch (Exception e) {
+			String password1 = actionRequest.getParameter("password1");
+			String password2 = actionRequest.getParameter("password2");
+
+			boolean submittedPassword = false;
+
+			if (e instanceof RequiredUserException ||
+				!Validator.isBlank(password1) ||
+				!Validator.isBlank(password2)) {
+
+				handleEditUserException(
+					actionRequest, actionResponse, submittedPassword);
+			}
+			else {
+				throw e;
+			}
+		}
+	}
+
+	public void updateLockout(
+		ActionRequest actionRequest,
+		ActionResponse actionResponse) throws Exception {
+
+		try {
+			User user = PortalUtil.getSelectedUser(actionRequest);
+
+			UserServiceUtil.updateLockoutById(user.getUserId(), false);
+
+			ThemeDisplay themeDisplay =
+					(ThemeDisplay)actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
+
+			String redirect = ParamUtil.getString(actionRequest, "redirect");
+
+			if (user != null) {
+				redirect = HttpUtil.setParameter(
+					redirect, actionResponse.getNamespace() + "p_u_i_d",
+					user.getUserId());
+			}
+
+			Group scopeGroup = themeDisplay.getScopeGroup();
+
+			if (scopeGroup.isUser() &&
+				(UserLocalServiceUtil.fetchUserById(
+					scopeGroup.getClassPK()) == null)) {
+
+				redirect = HttpUtil.setParameter(redirect, "doAsGroupId", 0);
+				redirect = HttpUtil.setParameter(redirect, "refererPlid", 0);
+			}
+
+			actionRequest.setAttribute(WebKeys.REDIRECT, redirect);
+		}
+		catch (Exception e) {
+			String password1 = actionRequest.getParameter("password1");
+			String password2 = actionRequest.getParameter("password2");
+
+			boolean submittedPassword = false;
+
+			if (e instanceof RequiredUserException ||
+				!Validator.isBlank(password1) ||
+				!Validator.isBlank(password2)) {
+
+				handleEditUserException(
+					actionRequest, actionResponse, submittedPassword);
+			}
+			else {
+				throw e;
+			}
+		}
 	}
 
 	public void updateOrganizationUserGroups(
@@ -172,6 +382,8 @@ public class UsersAdminPortlet extends MVCPortlet {
 			SessionErrors.contains(
 				renderRequest, NoSuchOrgLaborException.class.getName()) ||
 			SessionErrors.contains(
+				renderRequest, NoSuchUserException.class.getName()) ||
+			SessionErrors.contains(
 				renderRequest, PrincipalException.class.getName())) {
 
 			include("/error.jsp", renderRequest, renderResponse);
@@ -181,28 +393,83 @@ public class UsersAdminPortlet extends MVCPortlet {
 		}
 	}
 
+	protected void handleEditUserException(
+		ActionRequest actionRequest, ActionResponse actionResponse,
+		boolean submittedPassword) throws Exception {
+
+		String redirect = PortalUtil.escapeRedirect(
+			ParamUtil.getString(actionRequest, "redirect"));
+
+		if (submittedPassword) {
+			User user = PortalUtil.getSelectedUser(actionRequest);
+
+			redirect = HttpUtil.setParameter(
+				redirect, actionResponse.getNamespace() + "p_u_i_d",
+				user.getUserId());
+		}
+
+		if (Validator.isNotNull(redirect)) {
+			actionResponse.sendRedirect(redirect);
+		}
+	}
+
 	@Override
 	protected boolean isSessionErrorException(Throwable cause) {
 		if (cause instanceof AddressCityException ||
 			cause instanceof AddressStreetException ||
 			cause instanceof AddressZipException ||
+			cause instanceof CompanyMaxUsersException ||
+			cause instanceof ContactBirthdayException ||
+			cause instanceof ContactNameException ||
 			cause instanceof DuplicateOrganizationException ||
 			cause instanceof EmailAddressException ||
+			cause instanceof GroupFriendlyURLException ||
 			cause instanceof MembershipPolicyException ||
 			cause instanceof NoSuchCountryException ||
 			cause instanceof NoSuchListTypeException ||
 			cause instanceof NoSuchOrgLaborException ||
 			cause instanceof NoSuchRegionException ||
+			cause instanceof NoSuchUserException ||
 			cause instanceof OrganizationNameException ||
 			cause instanceof OrganizationParentException ||
 			cause instanceof PhoneNumberException ||
 			cause instanceof RequiredOrganizationException ||
+			cause instanceof RequiredUserException ||
+			cause instanceof UserEmailAddressException ||
+			cause instanceof UserFieldException ||
+			cause instanceof UserIdException ||
+			cause instanceof UserPasswordException ||
+			cause instanceof UserReminderQueryException ||
+			cause instanceof UserScreenNameException ||
+			cause instanceof UserSmsException ||
 			cause instanceof WebsiteURLException) {
 
 			return true;
 		}
 
 		return false;
+	}
+
+	protected void sendEditUserRedirect(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		String redirect = ParamUtil.getString(actionRequest, "redirect");
+
+		Group scopeGroup = themeDisplay.getScopeGroup();
+
+		if (scopeGroup.isUser() &&
+			(UserLocalServiceUtil.fetchUserById(
+				scopeGroup.getClassPK()) == null)) {
+
+			redirect = HttpUtil.setParameter(redirect, "doAsGroupId", 0);
+			redirect = HttpUtil.setParameter(redirect, "refererPlid", 0);
+		}
+
+		actionRequest.setAttribute(WebKeys.REDIRECT, redirect);
 	}
 
 }
