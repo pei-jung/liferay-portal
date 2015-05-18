58078e4bf1f82bfc2ec848fa9bf1ab9ad272604a
Pei-Jung Lan <pei-jung.lan@liferay.com>
2015-05-18 11:32:54 -0700

    LPS-55590 Convert EditOrganizationAction to ActionCommand

--- portal-impl/src/com/liferay/portlet/usersadmin/UsersAdminPortlet.java
+++ portal-impl/src/com/liferay/portlet/usersadmin/UsersAdminPortlet.java
@@ -14,11 +14,72 @@
 
 package com.liferay.portlet.usersadmin;
 
+import com.liferay.portal.AddressCityException;
+import com.liferay.portal.AddressStreetException;
+import com.liferay.portal.AddressZipException;
+import com.liferay.portal.DuplicateOrganizationException;
+import com.liferay.portal.EmailAddressException;
+import com.liferay.portal.NoSuchCountryException;
+import com.liferay.portal.NoSuchListTypeException;
+import com.liferay.portal.NoSuchOrganizationException;
+import com.liferay.portal.NoSuchRegionException;
+import com.liferay.portal.OrganizationNameException;
+import com.liferay.portal.OrganizationParentException;
+import com.liferay.portal.PhoneNumberException;
+import com.liferay.portal.RequiredOrganizationException;
+import com.liferay.portal.WebsiteURLException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.security.auth.PrincipalException;
+
+import java.io.IOException;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 /**
  * @author Pei-Jung Lan
  */
 public class UsersAdminPortlet extends MVCPortlet {
+
+	@Override
+	protected void doDispatch(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		if (SessionErrors.contains(
+				renderRequest, NoSuchOrganizationException.class.getName()) ||
+			SessionErrors.contains(
+				renderRequest, PrincipalException.class.getName())) {
+
+			include("/error.jsp", renderRequest, renderResponse);
+		}
+		else {
+			super.doDispatch(renderRequest, renderResponse);
+		}
+	}
+
+	@Override
+	protected boolean isSessionErrorException(Throwable cause) {
+		if (cause instanceof AddressCityException ||
+			cause instanceof AddressStreetException ||
+			cause instanceof AddressZipException ||
+			cause instanceof DuplicateOrganizationException ||
+			cause instanceof EmailAddressException ||
+			cause instanceof NoSuchCountryException ||
+			cause instanceof NoSuchListTypeException ||
+			cause instanceof NoSuchRegionException ||
+			cause instanceof OrganizationNameException ||
+			cause instanceof OrganizationParentException ||
+			cause instanceof PhoneNumberException ||
+			cause instanceof RequiredOrganizationException ||
+			cause instanceof WebsiteURLException) {
+
+			return true;
+		}
+
+		return false;
+	}
 
 }
