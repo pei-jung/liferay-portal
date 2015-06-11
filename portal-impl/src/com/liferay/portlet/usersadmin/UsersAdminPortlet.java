7a4b1985386a70b9bb7695dc8c0bcdb7b58e8c75
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2015-06-11 18:04:03 +0200

    LPS-55590 Makes the methods public, no logic changes

--- portal-impl/src/com/liferay/portlet/usersadmin/UsersAdminPortlet.java
+++ portal-impl/src/com/liferay/portlet/usersadmin/UsersAdminPortlet.java
@@ -55,6 +55,17 @@ import javax.portlet.RenderResponse;
  */
 public class UsersAdminPortlet extends MVCPortlet {
 
+	public void deleteOrganizations(ActionRequest actionRequest)
+		throws Exception {
+
+		long[] deleteOrganizationIds = StringUtil.split(
+			ParamUtil.getString(actionRequest, "deleteOrganizationIds"), 0L);
+
+		for (long deleteOrganizationId : deleteOrganizationIds) {
+			OrganizationServiceUtil.deleteOrganization(deleteOrganizationId);
+		}
+	}
+
 	public void deleteOrgLabor(
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
@@ -178,159 +189,7 @@ public class UsersAdminPortlet extends MVCPortlet {
 			getForward(renderRequest, "portlet.users_admin.edit_organization"));
 	}
 
-	public void updateOrganizationUserGroups(
-			ActionRequest actionRequest, ActionResponse actionResponse)
-		throws Exception {
-
-		long organizationId = ParamUtil.getLong(
-			actionRequest, "organizationId");
-
-		Organization organization =
-			OrganizationLocalServiceUtil.getOrganization(organizationId);
-
-		long groupId = organization.getGroupId();
-
-		long[] addUserGroupIds = StringUtil.split(
-			ParamUtil.getString(actionRequest, "addUserGroupIds"), 0L);
-		long[] removeUserGroupIds = StringUtil.split(
-			ParamUtil.getString(actionRequest, "removeUserGroupIds"), 0L);
-
-		UserGroupServiceUtil.addGroupUserGroups(groupId, addUserGroupIds);
-		UserGroupServiceUtil.unsetGroupUserGroups(groupId, removeUserGroupIds);
-
-		String redirect = ParamUtil.getString(
-			actionRequest, "assignmentsRedirect");
-
-		actionRequest.setAttribute(WebKeys.REDIRECT, redirect);
-	}
-
-	public void updateOrganizationUsers(
-			ActionRequest actionRequest, ActionResponse actionResponse)
-		throws Exception {
-
-		long organizationId = ParamUtil.getLong(
-			actionRequest, "organizationId");
-
-		long[] addUserIds = StringUtil.split(
-			ParamUtil.getString(actionRequest, "addUserIds"), 0L);
-		long[] removeUserIds = StringUtil.split(
-			ParamUtil.getString(actionRequest, "removeUserIds"), 0L);
-
-		UserServiceUtil.addOrganizationUsers(organizationId, addUserIds);
-		UserServiceUtil.unsetOrganizationUsers(organizationId, removeUserIds);
-
-		String redirect = ParamUtil.getString(
-			actionRequest, "assignmentsRedirect");
-
-		actionRequest.setAttribute(WebKeys.REDIRECT, redirect);
-	}
-
-	public void updateOrgLabor(
-			ActionRequest actionRequest, ActionResponse actionResponse)
-		throws Exception {
-
-		long orgLaborId = ParamUtil.getLong(actionRequest, "orgLaborId");
-
-		long organizationId = ParamUtil.getLong(
-			actionRequest, "organizationId");
-		long typeId = ParamUtil.getLong(actionRequest, "typeId");
-
-		int sunOpen = ParamUtil.getInteger(actionRequest, "sunOpen");
-		int sunClose = ParamUtil.getInteger(actionRequest, "sunClose");
-
-		int monOpen = ParamUtil.getInteger(actionRequest, "monOpen");
-		int monClose = ParamUtil.getInteger(actionRequest, "monClose");
-
-		int tueOpen = ParamUtil.getInteger(actionRequest, "tueOpen");
-		int tueClose = ParamUtil.getInteger(actionRequest, "tueClose");
-
-		int wedOpen = ParamUtil.getInteger(actionRequest, "wedOpen");
-		int wedClose = ParamUtil.getInteger(actionRequest, "wedClose");
-
-		int thuOpen = ParamUtil.getInteger(actionRequest, "thuOpen");
-		int thuClose = ParamUtil.getInteger(actionRequest, "thuClose");
-
-		int friOpen = ParamUtil.getInteger(actionRequest, "friOpen");
-		int friClose = ParamUtil.getInteger(actionRequest, "friClose");
-
-		int satOpen = ParamUtil.getInteger(actionRequest, "satOpen");
-		int satClose = ParamUtil.getInteger(actionRequest, "satClose");
-
-		if (orgLaborId <= 0) {
-
-			// Add organization labor
-
-			OrgLaborServiceUtil.addOrgLabor(
-				organizationId, typeId, sunOpen, sunClose, monOpen, monClose,
-				tueOpen, tueClose, wedOpen, wedClose, thuOpen, thuClose,
-				friOpen, friClose, satOpen, satClose);
-		}
-		else {
-
-			// Update organization labor
-
-			OrgLaborServiceUtil.updateOrgLabor(
-				orgLaborId, typeId, sunOpen, sunClose, monOpen, monClose,
-				tueOpen, tueClose, wedOpen, wedClose, thuOpen, thuClose,
-				friOpen, friClose, satOpen, satClose);
-		}
-	}
-
-	protected void deleteOrganizations(ActionRequest actionRequest)
-		throws Exception {
-
-		long[] deleteOrganizationIds = StringUtil.split(
-			ParamUtil.getString(actionRequest, "deleteOrganizationIds"), 0L);
-
-		for (long deleteOrganizationId : deleteOrganizationIds) {
-			OrganizationServiceUtil.deleteOrganization(deleteOrganizationId);
-		}
-	}
-
-	@Override
-	protected void doDispatch(
-			RenderRequest renderRequest, RenderResponse renderResponse)
-		throws IOException, PortletException {
-
-		if (SessionErrors.contains(
-				renderRequest, NoSuchOrganizationException.class.getName()) ||
-			SessionErrors.contains(
-				renderRequest, NoSuchOrgLaborException.class.getName()) ||
-			SessionErrors.contains(
-				renderRequest, PrincipalException.class.getName())) {
-
-			include("/error.jsp", renderRequest, renderResponse);
-		}
-		else {
-			super.doDispatch(renderRequest, renderResponse);
-		}
-	}
-
-	@Override
-	protected boolean isSessionErrorException(Throwable cause) {
-		if (cause instanceof AddressCityException ||
-			cause instanceof AddressStreetException ||
-			cause instanceof AddressZipException ||
-			cause instanceof DuplicateOrganizationException ||
-			cause instanceof EmailAddressException ||
-			cause instanceof MembershipPolicyException ||
-			cause instanceof NoSuchCountryException ||
-			cause instanceof NoSuchListTypeException ||
-			cause instanceof NoSuchOrgLaborException ||
-			cause instanceof NoSuchRegionException ||
-			cause instanceof OrganizationNameException ||
-			cause instanceof OrganizationParentException ||
-			cause instanceof PhoneNumberException ||
-			cause instanceof RequiredOrganizationException ||
-			cause instanceof WebsiteURLException) {
-
-			return true;
-		}
-
-		return false;
-	}
-
-	protected Organization updateOrganization(ActionRequest actionRequest)
+	public Organization updateOrganization(ActionRequest actionRequest)
 		throws Exception {
 
 		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
@@ -434,6 +293,147 @@ public class UsersAdminPortlet extends MVCPortlet {
 		portletPreferences.store();
 
 		return organization;
+	}
+
+	public void updateOrganizationUserGroups(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long organizationId = ParamUtil.getLong(
+			actionRequest, "organizationId");
+
+		Organization organization =
+			OrganizationLocalServiceUtil.getOrganization(organizationId);
+
+		long groupId = organization.getGroupId();
+
+		long[] addUserGroupIds = StringUtil.split(
+			ParamUtil.getString(actionRequest, "addUserGroupIds"), 0L);
+		long[] removeUserGroupIds = StringUtil.split(
+			ParamUtil.getString(actionRequest, "removeUserGroupIds"), 0L);
+
+		UserGroupServiceUtil.addGroupUserGroups(groupId, addUserGroupIds);
+		UserGroupServiceUtil.unsetGroupUserGroups(groupId, removeUserGroupIds);
+
+		String redirect = ParamUtil.getString(
+			actionRequest, "assignmentsRedirect");
+
+		actionRequest.setAttribute(WebKeys.REDIRECT, redirect);
+	}
+
+	public void updateOrganizationUsers(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long organizationId = ParamUtil.getLong(
+			actionRequest, "organizationId");
+
+		long[] addUserIds = StringUtil.split(
+			ParamUtil.getString(actionRequest, "addUserIds"), 0L);
+		long[] removeUserIds = StringUtil.split(
+			ParamUtil.getString(actionRequest, "removeUserIds"), 0L);
+
+		UserServiceUtil.addOrganizationUsers(organizationId, addUserIds);
+		UserServiceUtil.unsetOrganizationUsers(organizationId, removeUserIds);
+
+		String redirect = ParamUtil.getString(
+			actionRequest, "assignmentsRedirect");
+
+		actionRequest.setAttribute(WebKeys.REDIRECT, redirect);
+	}
+
+	public void updateOrgLabor(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long orgLaborId = ParamUtil.getLong(actionRequest, "orgLaborId");
+
+		long organizationId = ParamUtil.getLong(
+			actionRequest, "organizationId");
+		long typeId = ParamUtil.getLong(actionRequest, "typeId");
+
+		int sunOpen = ParamUtil.getInteger(actionRequest, "sunOpen");
+		int sunClose = ParamUtil.getInteger(actionRequest, "sunClose");
+
+		int monOpen = ParamUtil.getInteger(actionRequest, "monOpen");
+		int monClose = ParamUtil.getInteger(actionRequest, "monClose");
+
+		int tueOpen = ParamUtil.getInteger(actionRequest, "tueOpen");
+		int tueClose = ParamUtil.getInteger(actionRequest, "tueClose");
+
+		int wedOpen = ParamUtil.getInteger(actionRequest, "wedOpen");
+		int wedClose = ParamUtil.getInteger(actionRequest, "wedClose");
+
+		int thuOpen = ParamUtil.getInteger(actionRequest, "thuOpen");
+		int thuClose = ParamUtil.getInteger(actionRequest, "thuClose");
+
+		int friOpen = ParamUtil.getInteger(actionRequest, "friOpen");
+		int friClose = ParamUtil.getInteger(actionRequest, "friClose");
+
+		int satOpen = ParamUtil.getInteger(actionRequest, "satOpen");
+		int satClose = ParamUtil.getInteger(actionRequest, "satClose");
+
+		if (orgLaborId <= 0) {
+
+			// Add organization labor
+
+			OrgLaborServiceUtil.addOrgLabor(
+				organizationId, typeId, sunOpen, sunClose, monOpen, monClose,
+				tueOpen, tueClose, wedOpen, wedClose, thuOpen, thuClose,
+				friOpen, friClose, satOpen, satClose);
+		}
+		else {
+
+			// Update organization labor
+
+			OrgLaborServiceUtil.updateOrgLabor(
+				orgLaborId, typeId, sunOpen, sunClose, monOpen, monClose,
+				tueOpen, tueClose, wedOpen, wedClose, thuOpen, thuClose,
+				friOpen, friClose, satOpen, satClose);
+		}
+	}
+
+	@Override
+	protected void doDispatch(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		if (SessionErrors.contains(
+				renderRequest, NoSuchOrganizationException.class.getName()) ||
+			SessionErrors.contains(
+				renderRequest, NoSuchOrgLaborException.class.getName()) ||
+			SessionErrors.contains(
+				renderRequest, PrincipalException.class.getName())) {
+
+			include("/error.jsp", renderRequest, renderResponse);
+		}
+		else {
+			super.doDispatch(renderRequest, renderResponse);
+		}
+	}
+
+	@Override
+	protected boolean isSessionErrorException(Throwable cause) {
+		if (cause instanceof AddressCityException ||
+			cause instanceof AddressStreetException ||
+			cause instanceof AddressZipException ||
+			cause instanceof DuplicateOrganizationException ||
+			cause instanceof EmailAddressException ||
+			cause instanceof MembershipPolicyException ||
+			cause instanceof NoSuchCountryException ||
+			cause instanceof NoSuchListTypeException ||
+			cause instanceof NoSuchOrgLaborException ||
+			cause instanceof NoSuchRegionException ||
+			cause instanceof OrganizationNameException ||
+			cause instanceof OrganizationParentException ||
+			cause instanceof PhoneNumberException ||
+			cause instanceof RequiredOrganizationException ||
+			cause instanceof WebsiteURLException) {
+
+			return true;
+		}
+
+		return false;
 	}
 
 }
