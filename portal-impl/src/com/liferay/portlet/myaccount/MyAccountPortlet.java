ed42a5f2cf38a04ad733393ea6ef281e3f8c20e7
Pei-Jung Lan <pei-jung.lan@liferay.com>
2015-06-11 15:33:07 +0200

    LPS-55590 Convert ViewAction to MVCPortlet render

--- portal-impl/src/com/liferay/portlet/myaccount/MyAccountPortlet.java
+++ portal-impl/src/com/liferay/portlet/myaccount/MyAccountPortlet.java
@@ -14,10 +14,58 @@
 
 package com.liferay.portlet.myaccount;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.servlet.DynamicServletRequest;
+import com.liferay.portal.model.User;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.RenderRequestImpl;
 import com.liferay.portlet.usersadmin.UsersAdminPortlet;
+
+import java.io.IOException;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.WindowState;
 
 /**
  * @author Pei-Jung Lan
  */
 public class MyAccountPortlet extends UsersAdminPortlet {
+
+	@Override
+	public void render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		if ((renderRequest.getRemoteUser() == null) ||
+			!renderRequest.getWindowState().equals(WindowState.MAXIMIZED)) {
+
+			super.render(renderRequest, renderResponse);
+
+			return;
+		}
+
+		try {
+			User user = PortalUtil.getUser(renderRequest);
+
+			RenderRequestImpl renderRequestImpl =
+				(RenderRequestImpl)renderRequest;
+
+			DynamicServletRequest dynamicRequest =
+				(DynamicServletRequest)
+					renderRequestImpl.getHttpServletRequest();
+
+			dynamicRequest.setParameter(
+				"p_u_i_d", String.valueOf(user.getUserId()));
+
+			include(
+				"/html/portlet/my_account/edit_user.jsp", renderRequest,
+				renderResponse);
+		}
+		catch (PortalException pe) {
+			super.render(renderRequest, renderResponse);
+		}
+	}
+
 }
