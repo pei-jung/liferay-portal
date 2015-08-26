ac4abbb2ab06d0e73947bf990a31c668a203851c
drewbrokke <drew.brokke@liferay.com>
2015-08-26 16:22:04 -0700

    LPS-56950 adds new method to delete UserGroupGroupRoles by type

--- portal-impl/src/com/liferay/portal/service/impl/UserGroupGroupRoleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserGroupGroupRoleLocalServiceImpl.java
@@ -85,6 +85,22 @@ public class UserGroupGroupRoleLocalServiceImpl
 	}
 
 	@Override
+	public void deleteUserGroupGroupRoles(long groupId, int roleType)
+		throws PortalException {
+
+		List<UserGroupGroupRole> userGroupGroupRoles = getUserGroupGroupRoles(
+			groupId);
+
+		for (UserGroupGroupRole userGroupGroupRole : userGroupGroupRoles) {
+			Role role = userGroupGroupRole.getRole();
+
+			if (role.getType() == roleType) {
+				deleteUserGroupGroupRolesByRoleId(role.getRoleId());
+			}
+		}
+	}
+
+	@Override
 	public void deleteUserGroupGroupRoles(
 		long userGroupId, long groupId, long[] roleIds) {
 
