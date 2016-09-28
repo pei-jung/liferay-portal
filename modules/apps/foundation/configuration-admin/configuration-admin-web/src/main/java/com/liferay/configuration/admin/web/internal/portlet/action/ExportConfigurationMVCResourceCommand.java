6756d38f6ad9dd62e5f7a9172f9d576bdb10949a
drewbrokke <drew.brokke@liferay.com>
2016-09-28 09:40:07 -0700

    LPS-67890 changes file extension to ".config"

--- modules/apps/foundation/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/portlet/action/ExportConfigurationMVCResourceCommand.java
+++ modules/apps/foundation/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/portlet/action/ExportConfigurationMVCResourceCommand.java
@@ -225,7 +225,7 @@ public class ExportConfigurationMVCResourceCommand
 			fileName = factoryPid + StringPool.DASH + factoryInstanceId;
 		}
 
-		return fileName + ".cfg";
+		return fileName + ".config";
 	}
 
 	protected Properties getProperties(
