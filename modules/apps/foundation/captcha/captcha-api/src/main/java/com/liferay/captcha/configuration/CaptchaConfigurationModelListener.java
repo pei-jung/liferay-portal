1e172285212cd9276bf825c56eec59deb6a7755f
Pei-Jung Lan <pei-jung.lan@liferay.com>
2016-11-14 18:11:19 -0800

    modify CaptchaConfigurationModelListener

--- modules/apps/foundation/captcha/captcha-api/src/main/java/com/liferay/captcha/configuration/CaptchaConfigurationModelListener.java
+++ modules/apps/foundation/captcha/captcha-api/src/main/java/com/liferay/captcha/configuration/CaptchaConfigurationModelListener.java
@@ -14,28 +14,17 @@
 
 package com.liferay.captcha.configuration;
 
-import com.liferay.portal.captcha.CaptchaImpl;
 import com.liferay.captcha.recaptcha.ReCaptchaImpl;
-import com.liferay.captcha.simplecaptcha.SimpleCaptchaImpl;
 import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListener;
 import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListenerException;
-import com.liferay.portal.kernel.captcha.Captcha;
 import com.liferay.portal.kernel.captcha.CaptchaConfigurationException;
-import com.liferay.portal.kernel.captcha.CaptchaUtil;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.LocaleThreadLocal;
-import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.security.lang.DoPrivilegedBean;
-import com.liferay.portal.util.PrefsPropsUtil;
 
 import java.util.Dictionary;
 import java.util.Locale;
 import java.util.ResourceBundle;
-
-import javax.portlet.PortletPreferences;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -56,21 +45,15 @@ public class CaptchaConfigurationModelListener
 		throws ConfigurationModelListenerException {
 
 		try {
-			updateCaptcha(properties);
+			String captchaEngine = (String)properties.get("captchaEngine");
+
+			if (captchaEngine.equals(ReCaptchaImpl.class.getName())) {
+				validateReCaptchaKeys(properties);
+			}
 		}
 		catch (CaptchaConfigurationException cce) {
 			throw new ConfigurationModelListenerException(
 				cce.getMessage(), CaptchaConfiguration.class,
-				CaptchaConfigurationModelListener.class, properties);
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-
-			throw new ConfigurationModelListenerException(
-				ResourceBundleUtil.getString(
-					getResourceBundle(),
-					"an-error-occurred-while-updating-the-captcha-engine"),
-				CaptchaConfiguration.class,
 				CaptchaConfigurationModelListener.class, properties);
 		}
 	}
@@ -86,61 +69,13 @@ public class CaptchaConfigurationModelListener
 		return _resourceBundle;
 	}
 
-	protected void updateCaptcha(Dictionary<String, Object> properties)
-		throws Exception {
+	protected void validateReCaptchaKeys(Dictionary<String, Object> properties)
+		throws CaptchaConfigurationException {
 
-		String captchaEngine = (String)properties.get("captchaEngine");
 		String reCaptchaPublicKey = (String)properties.get(
 			"reCaptchaPublicKey");
 		String reCaptchaPrivateKey = (String)properties.get(
 			"reCaptchaPrivateKey");
-
-		Captcha captcha = null;
-
-		if (captchaEngine.equals(SimpleCaptchaImpl.class.getName())) {
-			captcha = new SimpleCaptchaImpl();
-		}
-		else {
-			captcha = new ReCaptchaImpl();
-
-			validateReCaptchaKeys(reCaptchaPublicKey, reCaptchaPrivateKey);
-		}
-
-		PortletPreferences portletPreferences = PrefsPropsUtil.getPreferences();
-
-		Class<?> clazz = captcha.getClass();
-
-		portletPreferences.setValue(
-			PropsKeys.CAPTCHA_ENGINE_IMPL, clazz.getName());
-
-		portletPreferences.setValue(
-			PropsKeys.CAPTCHA_ENGINE_RECAPTCHA_KEY_PRIVATE,
-			reCaptchaPrivateKey);
-		portletPreferences.setValue(
-			PropsKeys.CAPTCHA_ENGINE_RECAPTCHA_KEY_PUBLIC, reCaptchaPublicKey);
-
-		portletPreferences.store();
-
-		CaptchaImpl captchaImpl = null;
-
-		Captcha currentCaptcha = CaptchaUtil.getCaptcha();
-
-		if (currentCaptcha instanceof DoPrivilegedBean) {
-			DoPrivilegedBean doPrivilegedBean =
-				(DoPrivilegedBean)currentCaptcha;
-
-			captchaImpl = (CaptchaImpl)doPrivilegedBean.getActualBean();
-		}
-		else {
-			captchaImpl = (CaptchaImpl)currentCaptcha;
-		}
-
-		captchaImpl.setCaptcha(captcha);
-	}
-
-	protected void validateReCaptchaKeys(
-			String reCaptchaPublicKey, String reCaptchaPrivateKey)
-		throws Exception {
 
 		if (Validator.isNull(reCaptchaPublicKey)) {
 			throw new CaptchaConfigurationException(
@@ -156,9 +91,6 @@ public class CaptchaConfigurationModelListener
 					"the-recaptcha-private-key-is-not-valid"));
 		}
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		CaptchaConfigurationModelListener.class);
 
 	private ResourceBundle _resourceBundle;
 
