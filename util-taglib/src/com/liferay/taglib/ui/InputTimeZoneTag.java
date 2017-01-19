e3d4a018aaccea1923805795561d2b119e915cd2
drewbrokke <drew.brokke@liferay.com>
2017-01-18 16:04:12 -0800

    LPS-70161 refactor - util-taglib - InputTimeZoneTag.java - move logic into Tag class

--- util-taglib/src/com/liferay/taglib/ui/InputTimeZoneTag.java
+++ util-taglib/src/com/liferay/taglib/ui/InputTimeZoneTag.java
@@ -14,10 +14,26 @@
 
 package com.liferay.taglib.ui;
 
+import com.liferay.ibm.icu.text.TimeZoneFormat;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Time;
+import com.liferay.portal.kernel.util.TimeZoneComparator;
 import com.liferay.portal.kernel.util.TimeZoneUtil;
 import com.liferay.taglib.util.IncludeTag;
 
+import java.text.NumberFormat;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
 import java.util.TimeZone;
+import java.util.TreeSet;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -64,6 +80,26 @@ public class InputTimeZoneTag extends IncludeTag {
 		_value = value;
 	}
 
+	public class TimeZoneDisplay {
+
+		public TimeZoneDisplay(String displayName, String timeZoneId) {
+			_displayName = displayName;
+			_timeZoneId = timeZoneId;
+		}
+
+		public String getDisplayName() {
+			return _displayName;
+		}
+
+		public String getTimeZoneId() {
+			return _timeZoneId;
+		}
+
+		private final String _displayName;
+		private final String _timeZoneId;
+
+	}
+
 	@Override
 	protected void cleanUp() {
 		_autoFocus = false;
@@ -100,6 +136,100 @@ public class InputTimeZoneTag extends IncludeTag {
 		request.setAttribute(
 			"liferay-ui:input-time-zone:nullable", String.valueOf(_nullable));
 		request.setAttribute("liferay-ui:input-time-zone:value", _value);
+
+		request.setAttribute(
+			"liferay-ui:input-time-zone:timeZoneDisplays",
+			_getTimeZoneDisplays(request));
+	}
+
+	private TimeZoneDisplay _getTimeZoneDisplay(
+		Date date, Locale locale, long currentTime, NumberFormat numberFormat,
+		TimeZone timeZone) {
+
+		StringBundler displayNameSb = new StringBundler();
+
+		int totalOffset = timeZone.getOffset(currentTime);
+
+		if (totalOffset != 0) {
+			displayNameSb.append(StringPool.OPEN_PARENTHESIS);
+			displayNameSb.append("UTC");
+
+			String offsetHour = numberFormat.format(totalOffset / Time.HOUR);
+			String offsetMinute = numberFormat.format(
+				Math.abs(totalOffset % Time.HOUR) / Time.MINUTE);
+
+			displayNameSb.append(StringPool.SPACE);
+
+			if (totalOffset > 0) {
+				displayNameSb.append(StringPool.PLUS);
+			}
+
+			displayNameSb.append(offsetHour);
+			displayNameSb.append(StringPool.COLON);
+			displayNameSb.append(offsetMinute);
+			displayNameSb.append(StringPool.CLOSE_PARENTHESIS);
+			displayNameSb.append(StringPool.SPACE);
+		}
+
+		displayNameSb.append(
+			timeZone.getDisplayName(
+				timeZone.inDaylightTime(date), _displayStyle, locale));
+
+		String timeZoneId = timeZone.getID();
+
+		if (timeZoneId.contains("Phoenix")) {
+			StringBundler sb = new StringBundler(4);
+
+			displayNameSb.append(StringPool.SPACE);
+			displayNameSb.append(StringPool.OPEN_PARENTHESIS);
+
+			com.liferay.ibm.icu.util.TimeZone icuTimeZone =
+				com.liferay.ibm.icu.util.TimeZone.getTimeZone(timeZoneId);
+
+			com.liferay.ibm.icu.text.SimpleDateFormat icuSimpleDateFormat =
+				new com.liferay.ibm.icu.text.SimpleDateFormat();
+
+			TimeZoneFormat icuTimeZoneFormat =
+				icuSimpleDateFormat.getTimeZoneFormat();
+
+			displayNameSb.append(
+				icuTimeZoneFormat.format(
+					TimeZoneFormat.Style.ZONE_ID, icuTimeZone, date.getTime()));
+
+			displayNameSb.append(StringPool.CLOSE_PARENTHESIS);
+		}
+
+		return new TimeZoneDisplay(displayNameSb.toString(), timeZoneId);
+	}
+
+	private List<TimeZoneDisplay> _getTimeZoneDisplays(
+		HttpServletRequest request) {
+
+		List<TimeZoneDisplay> timeZoneDisplays = new ArrayList();
+
+		Set<TimeZone> timeZones = new TreeSet(new TimeZoneComparator());
+
+		for (String timeZoneId : PropsUtil.getArray(PropsKeys.TIME_ZONES)) {
+			TimeZone curTimeZone = TimeZoneUtil.getTimeZone(timeZoneId);
+
+			timeZones.add(curTimeZone);
+		}
+
+		long currentTime = System.currentTimeMillis();
+		Date date = new Date();
+		Locale locale = PortalUtil.getLocale(request);
+
+		NumberFormat numberFormat = NumberFormat.getInstance(locale);
+
+		numberFormat.setMinimumIntegerDigits(2);
+
+		for (TimeZone timeZone : timeZones) {
+			timeZoneDisplays.add(
+				_getTimeZoneDisplay(
+					date, locale, currentTime, numberFormat, timeZone));
+		}
+
+		return timeZoneDisplays;
 	}
 
 	private static final String _PAGE =
