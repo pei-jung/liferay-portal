f226abc0fdbe6a738ef6debdc6fb8a09c98bfe6b
Noah Sherrill <noah.sherrill@liferay.com>
2018-01-10 13:56:35 -0800

    LPS-76030 Implement GDPR for BookmarksEntry data

---
+++ modules/apps/collaboration/bookmarks/bookmarks-uad/src/main/java/com/liferay/bookmarks/uad/aggregator/BookmarksEntryUADEntityAggregator.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.bookmarks.uad.aggregator;
+
+import com.liferay.bookmarks.model.BookmarksEntry;
+import com.liferay.bookmarks.service.BookmarksEntryLocalService;
+import com.liferay.bookmarks.uad.constants.BookmarksUADConstants;
+import com.liferay.bookmarks.uad.entity.BookmarksEntryUADEntity;
+import com.liferay.portal.kernel.dao.orm.Criterion;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.user.associated.data.aggregator.BaseUADEntityAggregator;
+import com.liferay.user.associated.data.aggregator.UADEntityAggregator;
+import com.liferay.user.associated.data.entity.UADEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Noah Sherrill
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=" + BookmarksUADConstants.BOOKMARKS_ENTRY,
+	service = UADEntityAggregator.class
+)
+public class BookmarksEntryUADEntityAggregator extends BaseUADEntityAggregator {
+
+	@Override
+	public List<UADEntity> getUADEntities(long userId) {
+		DynamicQuery dynamicQuery = _bookmarksEntryLocalService.dynamicQuery();
+
+		Criterion statusByUserIdCriterion = RestrictionsFactoryUtil.eq(
+			"statusByUserId", userId);
+		Criterion userIdCriterion = RestrictionsFactoryUtil.eq(
+			"userId", userId);
+
+		dynamicQuery.add(
+			RestrictionsFactoryUtil.or(
+				statusByUserIdCriterion, userIdCriterion));
+
+		List<BookmarksEntry> bookmarksEntries =
+			_bookmarksEntryLocalService.dynamicQuery(dynamicQuery);
+
+		List<UADEntity> uadEntities = new ArrayList<>(bookmarksEntries.size());
+
+		for (BookmarksEntry bookmarksEntry : bookmarksEntries) {
+			uadEntities.add(
+				_createBookmarksEntryUADEntity(userId, bookmarksEntry));
+		}
+
+		return uadEntities;
+	}
+
+	@Override
+	public UADEntity getUADEntity(String uadEntityId) throws PortalException {
+		BookmarksEntry bookmarksEntry =
+			_bookmarksEntryLocalService.getBookmarksEntry(
+				_getEntryId(uadEntityId));
+
+		return _createBookmarksEntryUADEntity(
+			_getUserId(uadEntityId), bookmarksEntry);
+	}
+
+	private BookmarksEntryUADEntity _createBookmarksEntryUADEntity(
+		long userId, BookmarksEntry bookmarksEntry) {
+
+		return new BookmarksEntryUADEntity(
+			userId, _getUADEntityId(userId, bookmarksEntry), bookmarksEntry);
+	}
+
+	private long _getEntryId(String uadEntityId) {
+		String[] uadEntityIdParts = uadEntityId.split(StringPool.POUND);
+
+		return Long.parseLong(uadEntityIdParts[0]);
+	}
+
+	private String _getUADEntityId(long userId, BookmarksEntry bookmarksEntry) {
+		return Long.toString(bookmarksEntry.getEntryId()) + StringPool.POUND +
+			Long.toString(userId);
+	}
+
+	private long _getUserId(String uadEntityId) {
+		String[] uadEntityIdParts = uadEntityId.split(StringPool.POUND);
+
+		return Long.parseLong(uadEntityIdParts[1]);
+	}
+
+	@Reference
+	private BookmarksEntryLocalService _bookmarksEntryLocalService;
+
+}
