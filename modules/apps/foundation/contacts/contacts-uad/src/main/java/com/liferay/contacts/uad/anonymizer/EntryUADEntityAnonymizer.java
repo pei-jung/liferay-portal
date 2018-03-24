1cd006f415212debb7e9884e49d10126ab4343ae
drewbrokke <drew.brokke@liferay.com>
2018-03-23 18:18:22 -0700

    LPS-78886 contacts - autogenerated

--- modules/apps/foundation/contacts/contacts-uad/src/main/java/com/liferay/contacts/uad/anonymizer/EntryUADEntityAnonymizer.java
+++ modules/apps/foundation/contacts/contacts-uad/src/main/java/com/liferay/contacts/uad/anonymizer/EntryUADEntityAnonymizer.java
@@ -17,19 +17,14 @@ package com.liferay.contacts.uad.anonymizer;
 import com.liferay.contacts.model.Entry;
 import com.liferay.contacts.service.EntryLocalService;
 import com.liferay.contacts.uad.constants.ContactsUADConstants;
-import com.liferay.contacts.uad.entity.EntryUADEntity;
 
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.User;
 
-import com.liferay.user.associated.data.aggregator.UADEntityAggregator;
-import com.liferay.user.associated.data.anonymizer.BaseUADEntityAnonymizer;
+import com.liferay.user.associated.data.anonymizer.DynamicQueryUADEntityAnonymizer;
 import com.liferay.user.associated.data.anonymizer.UADEntityAnonymizer;
-import com.liferay.user.associated.data.entity.UADEntity;
-import com.liferay.user.associated.data.exception.UADEntityException;
 import com.liferay.user.associated.data.util.UADAnonymizerHelper;
-import com.liferay.user.associated.data.util.UADDynamicQueryHelper;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -43,60 +38,19 @@ import java.util.List;
  */
 @Component(immediate = true, property =  {
 	"model.class.name=" + ContactsUADConstants.CLASS_NAME_ENTRY}, service = UADEntityAnonymizer.class)
-public class EntryUADEntityAnonymizer extends BaseUADEntityAnonymizer {
-	@Override
-	public void autoAnonymize(UADEntity uadEntity) throws PortalException {
-		Entry entry = _getEntry(uadEntity);
-
-		_autoAnonymize(entry, uadEntity.getUserId());
-	}
-
-	@Override
-	public void autoAnonymizeAll(final long userId) throws PortalException {
-		ActionableDynamicQuery actionableDynamicQuery = _getActionableDynamicQuery(userId);
-
-		actionableDynamicQuery.setPerformActionMethod(new ActionableDynamicQuery.PerformActionMethod<Entry>() {
-				@Override
-				public void performAction(Entry entry)
-					throws PortalException {
-					_autoAnonymize(entry, userId);
-				}
-			});
-
-		actionableDynamicQuery.performActions();
-	}
-
-	@Override
-	public void delete(UADEntity uadEntity) throws PortalException {
-		_entryLocalService.deleteEntry(_getEntry(uadEntity));
-	}
-
-	@Override
-	public void deleteAll(long userId) throws PortalException {
-		ActionableDynamicQuery actionableDynamicQuery = _getActionableDynamicQuery(userId);
-
-		actionableDynamicQuery.setPerformActionMethod(new ActionableDynamicQuery.PerformActionMethod<Entry>() {
-				@Override
-				public void performAction(Entry entry)
-					throws PortalException {
-					_entryLocalService.deleteEntry(entry);
-				}
-			});
-
-		actionableDynamicQuery.performActions();
-	}
-
-	@Override
-	protected UADEntityAggregator getUADEntityAggregator() {
-		return _uadEntityAggregator;
-	}
-
+public class EntryUADEntityAnonymizer extends DynamicQueryUADEntityAnonymizer<Entry> {
 	@Override
 	public List<String> getUADEntityNonanonymizableFieldNames() {
-		return Arrays.asList("fullName", "emailAddress", "comments");
+		return Arrays.asList("description", "name", "url");
 	}
 
-	private void _autoAnonymize(Entry entry, long userId)
+	@Override
+	public String[] getUserIdFieldNames() {
+		return ContactsUADConstants.USER_ID_FIELD_NAMES_ENTRY;
+	}
+
+	@Override
+	protected void doAutoAnonymize(Entry entry, long userId)
 		throws PortalException {
 		User anonymousUser = _uadAnonymizerHelper.getAnonymousUser();
 
@@ -108,32 +62,18 @@ public class EntryUADEntityAnonymizer extends BaseUADEntityAnonymizer {
 		_entryLocalService.updateEntry(entry);
 	}
 
-	private ActionableDynamicQuery _getActionableDynamicQuery(long userId) {
-		return _uadDynamicQueryHelper.addActionableDynamicQueryCriteria(_entryLocalService.getActionableDynamicQuery(),
-			ContactsUADConstants.USER_ID_FIELD_NAMES_ENTRY, userId);
+	@Override
+	protected void doDelete(Entry entry) throws PortalException {
+		_entryLocalService.deleteEntry(entry);
 	}
 
-	private Entry _getEntry(UADEntity uadEntity) throws PortalException {
-		_validate(uadEntity);
-
-		EntryUADEntity entryUADEntity = (EntryUADEntity)uadEntity;
-
-		return entryUADEntity.getEntry();
-	}
-
-	private void _validate(UADEntity uadEntity) throws PortalException {
-		if (!(uadEntity instanceof EntryUADEntity)) {
-			throw new UADEntityException();
-		}
+	@Override
+	protected ActionableDynamicQuery doGetActionableDynamicQuery() {
+		return _entryLocalService.getActionableDynamicQuery();
 	}
 
 	@Reference
 	private EntryLocalService _entryLocalService;
 	@Reference
 	private UADAnonymizerHelper _uadAnonymizerHelper;
-	@Reference
-	private UADDynamicQueryHelper _uadDynamicQueryHelper;
-	@Reference(target = "(model.class.name=" +
-	ContactsUADConstants.CLASS_NAME_ENTRY + ")")
-	private UADEntityAggregator _uadEntityAggregator;
 }
