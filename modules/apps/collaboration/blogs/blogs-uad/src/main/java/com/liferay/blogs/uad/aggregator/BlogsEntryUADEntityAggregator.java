ac2c040b845c17ffffe37a11a98ac6dc077b3b34
drewbrokke <drew.brokke@liferay.com>
2018-03-23 18:15:15 -0700

    LPS-78886 blogs - autogenerated

--- modules/apps/collaboration/blogs/blogs-uad/src/main/java/com/liferay/blogs/uad/aggregator/BlogsEntryUADEntityAggregator.java
+++ modules/apps/collaboration/blogs/blogs-uad/src/main/java/com/liferay/blogs/uad/aggregator/BlogsEntryUADEntityAggregator.java
@@ -17,20 +17,16 @@ package com.liferay.blogs.uad.aggregator;
 import com.liferay.blogs.model.BlogsEntry;
 import com.liferay.blogs.service.BlogsEntryLocalService;
 import com.liferay.blogs.uad.constants.BlogsUADConstants;
-import com.liferay.blogs.uad.entity.BlogsEntryUADEntity;
 
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
 import com.liferay.portal.kernel.exception.PortalException;
 
-import com.liferay.user.associated.data.aggregator.BaseUADEntityAggregator;
+import com.liferay.user.associated.data.aggregator.DynamicQueryUADEntityAggregator;
 import com.liferay.user.associated.data.aggregator.UADEntityAggregator;
-import com.liferay.user.associated.data.entity.UADEntity;
-import com.liferay.user.associated.data.util.UADDynamicQueryHelper;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -39,66 +35,54 @@ import java.util.List;
  */
 @Component(immediate = true, property =  {
 	"model.class.name=" + BlogsUADConstants.CLASS_NAME_BLOGS_ENTRY}, service = UADEntityAggregator.class)
-public class BlogsEntryUADEntityAggregator extends BaseUADEntityAggregator {
-	@Override
-	public int count(long userId) {
-		return (int)_blogsEntryLocalService.dynamicQueryCount(_getDynamicQuery(
-				userId));
-	}
-
-	@Override
-	public List<UADEntity> getUADEntities(long userId, int start, int end) {
-		List<BlogsEntry> blogsEntries = _blogsEntryLocalService.dynamicQuery(_getDynamicQuery(
-					userId), start, end);
-
-		List<UADEntity> uadEntities = new ArrayList<UADEntity>(blogsEntries.size());
-
-		for (BlogsEntry blogsEntry : blogsEntries) {
-			uadEntities.add(new BlogsEntryUADEntity(userId,
-					_getUADEntityId(userId, blogsEntry), blogsEntry));
-		}
-
-		return uadEntities;
-	}
-
-	@Override
-	public UADEntity getUADEntity(String uadEntityId) throws PortalException {
-		BlogsEntry blogsEntry = _blogsEntryLocalService.getBlogsEntry(_getEntryId(
-					uadEntityId));
-
-		return new BlogsEntryUADEntity(_getUserId(uadEntityId), uadEntityId,
-			blogsEntry);
-	}
-
+public class BlogsEntryUADEntityAggregator
+	extends DynamicQueryUADEntityAggregator<BlogsEntry> {
 	@Override
 	public String getUADEntitySetName() {
 		return BlogsUADConstants.UAD_ENTITY_SET_NAME;
 	}
 
-	private DynamicQuery _getDynamicQuery(long userId) {
-		return _uadDynamicQueryHelper.addDynamicQueryCriteria(_blogsEntryLocalService.dynamicQuery(),
-			BlogsUADConstants.USER_ID_FIELD_NAMES_BLOGS_ENTRY, userId);
+	@Override
+	public String[] getUserIdFieldNames() {
+		return BlogsUADConstants.USER_ID_FIELD_NAMES_BLOGS_ENTRY;
 	}
 
-	private long _getEntryId(String uadEntityId) {
-		String[] uadEntityIdParts = uadEntityId.split("#");
-
-		return Long.parseLong(uadEntityIdParts[0]);
+	@Override
+	protected long doCount(DynamicQuery dynamicQuery) {
+		return _blogsEntryLocalService.dynamicQueryCount(dynamicQuery);
 	}
 
-	private String _getUADEntityId(long userId, BlogsEntry blogsEntry) {
-		return String.valueOf(blogsEntry.getEntryId()) + "#" +
-		String.valueOf(userId);
+	@Override
+	protected DynamicQuery doGetDynamicQuery() {
+		return _blogsEntryLocalService.dynamicQuery();
 	}
 
-	private long _getUserId(String uadEntityId) {
-		String[] uadEntityIdParts = uadEntityId.split("#");
+	@Override
+	protected List<BlogsEntry> doGetEntities(DynamicQuery dynamicQuery,
+		int start, int end) {
+		return _blogsEntryLocalService.dynamicQuery(dynamicQuery, start, end);
+	}
 
-		return Long.parseLong(uadEntityIdParts[1]);
+	@Override
+	protected BlogsEntry doGetEntity(long entityId) throws PortalException {
+		return _blogsEntryLocalService.getBlogsEntry(entityId);
+	}
+
+	@Override
+	protected long doGetEntityId(BlogsEntry blogsEntry) {
+		return blogsEntry.getEntryId();
+	}
+
+	@Override
+	protected String doGetRegistryKey() {
+		return BlogsUADConstants.CLASS_NAME_BLOGS_ENTRY;
+	}
+
+	@Override
+	protected long doGetUserId(BlogsEntry blogsEntry) {
+		return blogsEntry.getUserId();
 	}
 
 	@Reference
 	private BlogsEntryLocalService _blogsEntryLocalService;
-	@Reference
-	private UADDynamicQueryHelper _uadDynamicQueryHelper;
 }
