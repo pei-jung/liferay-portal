7ca07f7360b3f9deb46a5ba9d397628a33d0e19d
Samuel Tran <samuel.tran@liferay.com>
2019-03-15 17:25:46 -0700

    LPS-90259 Embed personal application portlets in a user page

--- modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/portlet/MyAccountPortlet.java
+++ modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/portlet/MyAccountPortlet.java
@@ -38,7 +38,6 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.private-session-attributes=false",
 		"com.liferay.portlet.render-weight=50",
 		"com.liferay.portlet.struts-path=users_admin",
-		"com.liferay.portlet.system=true",
 		"com.liferay.portlet.use-default-template=true",
 		"javax.portlet.display-name=My Account",
 		"javax.portlet.expiration-cache=0",
