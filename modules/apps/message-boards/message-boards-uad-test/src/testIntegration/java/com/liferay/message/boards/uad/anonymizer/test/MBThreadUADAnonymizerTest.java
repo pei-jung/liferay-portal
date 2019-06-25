3fe4800fe91bfa3d0be38d47b8b9702692b7abf5
Samuel Tran <samuel.tran@liferay.com>
2019-06-25 16:54:13 -0700

    LPS-97094 message-boards-uad-test - test for anonymized AssetEntry

--- modules/apps/message-boards/message-boards-uad-test/src/testIntegration/java/com/liferay/message/boards/uad/anonymizer/test/MBThreadUADAnonymizerTest.java
+++ modules/apps/message-boards/message-boards-uad-test/src/testIntegration/java/com/liferay/message/boards/uad/anonymizer/test/MBThreadUADAnonymizerTest.java
@@ -26,7 +26,7 @@ import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.user.associated.data.anonymizer.UADAnonymizer;
-import com.liferay.user.associated.data.test.util.BaseUADAnonymizerTestCase;
+import com.liferay.user.associated.data.test.util.BaseHasAssetEntryUADAnonymizerTestCase;
 import com.liferay.user.associated.data.test.util.WhenHasStatusByUserIdField;
 
 import java.util.ArrayList;
@@ -42,7 +42,7 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Arquillian.class)
 public class MBThreadUADAnonymizerTest
-	extends BaseUADAnonymizerTestCase<MBThread>
+	extends BaseHasAssetEntryUADAnonymizerTestCase<MBThread>
 	implements WhenHasStatusByUserIdField {
 
 	@ClassRule
@@ -119,7 +119,11 @@ public class MBThreadUADAnonymizerTest
 			(mbThread.getStatusByUserId() != user.getUserId()) &&
 			!statusByUserName.equals(user.getFullName())) {
 
-			return true;
+			if (isAssetEntryAutoAnonymized(
+					MBThread.class.getName(), mbThread.getThreadId(), user)) {
+
+				return true;
+			}
 		}
 
 		return false;
