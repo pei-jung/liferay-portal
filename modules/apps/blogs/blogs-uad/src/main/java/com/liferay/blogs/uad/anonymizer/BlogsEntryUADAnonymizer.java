731d1a3ac4b53c29f3e06dabc9c186fbf146f22b
Samuel Tran <samuel.tran@liferay.com>
2019-06-26 15:28:53 -0700

    LPS-97094 blogs-uad - anonymize asset entry

--- modules/apps/blogs/blogs-uad/src/main/java/com/liferay/blogs/uad/anonymizer/BlogsEntryUADAnonymizer.java
+++ modules/apps/blogs/blogs-uad/src/main/java/com/liferay/blogs/uad/anonymizer/BlogsEntryUADAnonymizer.java
@@ -14,13 +14,40 @@
 
 package com.liferay.blogs.uad.anonymizer;
 
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.service.AssetEntryLocalService;
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.user.associated.data.anonymizer.UADAnonymizer;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
  */
 @Component(immediate = true, service = UADAnonymizer.class)
 public class BlogsEntryUADAnonymizer extends BaseBlogsEntryUADAnonymizer {
+	@Override
+	public void autoAnonymize(
+		BlogsEntry blogsEntry, long userId, User anonymousUser)
+		throws PortalException {
+
+		if (blogsEntry.getUserId() == userId) {
+			AssetEntry assetEntry = _assetEntryLocalService.fetchEntry(
+				BlogsEntry.class.getName(), blogsEntry.getEntryId());
+
+			assetEntry.setUserId(anonymousUser.getUserId());
+			assetEntry.setUserName(anonymousUser.getFullName());
+
+			_assetEntryLocalService.updateAssetEntry(assetEntry);
+		}
+
+		super.autoAnonymize(blogsEntry, userId, anonymousUser);
+	}
+
+	@Reference
+	private AssetEntryLocalService _assetEntryLocalService;
+
 }
