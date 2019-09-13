2b8f0f1968317a6e675c59c27ff27689e05af638
alee8888 <albert.lee@liferay.com>
2019-09-13 09:33:55 -0700

    LPS-101269 Add Management Toolbar display context class for viewing Users

---
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/display/context/ViewAccountsUsersManagementToolbarDisplayContext.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.admin.web.internal.display.context;
+
+import com.liferay.frontend.taglib.clay.servlet.taglib.display.context.SearchContainerManagementToolbarDisplayContext;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.CreationMenu;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.portlet.PortletURLUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Albert Lee
+ */
+public class ViewAccountsUsersManagementToolbarDisplayContext
+	extends SearchContainerManagementToolbarDisplayContext {
+
+	public ViewAccountsUsersManagementToolbarDisplayContext(
+		HttpServletRequest httpServletRequest,
+		LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse,
+		SearchContainer searchContainer) {
+
+		super(
+			liferayPortletRequest, liferayPortletResponse, httpServletRequest,
+			searchContainer);
+	}
+
+	@Override
+	public String getClearResultsURL() {
+		PortletURL clearResultsURL = getPortletURL();
+
+		clearResultsURL.setParameter("navigation", (String)null);
+		clearResultsURL.setParameter("keywords", StringPool.BLANK);
+
+		return clearResultsURL.toString();
+	}
+
+	public CreationMenu getCreationMenu() {
+		return new CreationMenu() {
+			{
+				addPrimaryDropdownItem(
+					dropdownItem -> {
+						dropdownItem.setHref(
+							liferayPortletResponse.createRenderURL(),
+							"mvcRenderCommandName",
+							"/account_admin/account/add_user", "accountEntryId",
+							ParamUtil.getLong(
+								liferayPortletRequest, "accountEntryId"));
+						dropdownItem.setLabel(
+							LanguageUtil.get(request, "add-user"));
+					});
+			}
+		};
+	}
+
+	@Override
+	public PortletURL getPortletURL() {
+		try {
+			return PortletURLUtil.clone(currentURLObj, liferayPortletResponse);
+		}
+		catch (Exception e) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(e, e);
+			}
+
+			return liferayPortletResponse.createRenderURL();
+		}
+	}
+
+	@Override
+	public Boolean isShowCreationMenu() {
+		return true;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ViewAccountsUsersManagementToolbarDisplayContext.class);
+
+}
