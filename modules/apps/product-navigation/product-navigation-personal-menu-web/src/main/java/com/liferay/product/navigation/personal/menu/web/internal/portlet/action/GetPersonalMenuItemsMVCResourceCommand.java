5bf196fbc97fa4c16d5eaa9fed2ed51f081c2a22
drewbrokke <drew.brokke@liferay.com>
2019-11-04 14:07:12 -0600

    LPS-102069 product-navigation-personal-menu-web - (GetPersonalMenuItemsMVCResourceCommand.java) make sure that the real user has their embedded personal application layout before trying to navigate there

--- modules/apps/product-navigation/product-navigation-personal-menu-web/src/main/java/com/liferay/product/navigation/personal/menu/web/internal/portlet/action/GetPersonalMenuItemsMVCResourceCommand.java
+++ modules/apps/product-navigation/product-navigation-personal-menu-web/src/main/java/com/liferay/product/navigation/personal/menu/web/internal/portlet/action/GetPersonalMenuItemsMVCResourceCommand.java
@@ -37,6 +37,7 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.product.navigation.personal.menu.PersonalMenuEntry;
 import com.liferay.product.navigation.personal.menu.constants.PersonalMenuPortletKeys;
+import com.liferay.product.navigation.personal.menu.util.PersonalApplicationURLUtil;
 import com.liferay.product.navigation.personal.menu.web.internal.PersonalMenuEntryRegistry;
 
 import java.util.List;
@@ -104,6 +105,9 @@ public class GetPersonalMenuItemsMVCResourceCommand
 			realUserURL = StringUtil.replace(
 				realUserURL, userProfileURL,
 				_http.getPath(realUser.getDisplayURL(themeDisplay, false)));
+
+			PersonalApplicationURLUtil.getEmbeddedPersonalApplicationLayout(
+				realUser, realUser.getGroup(), false);
 		}
 
 		String userDashboardURL = _http.getPath(
@@ -113,6 +117,9 @@ public class GetPersonalMenuItemsMVCResourceCommand
 			realUserURL = StringUtil.replace(
 				realUserURL, userDashboardURL,
 				_http.getPath(realUser.getDisplayURL(themeDisplay, true)));
+
+			PersonalApplicationURLUtil.getEmbeddedPersonalApplicationLayout(
+				realUser, realUser.getGroup(), true);
 		}
 
 		JSONObject jsonObject1 = JSONUtil.put(
