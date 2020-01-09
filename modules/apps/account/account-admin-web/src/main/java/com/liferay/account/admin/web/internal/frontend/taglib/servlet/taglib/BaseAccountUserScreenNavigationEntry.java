436c1f15f0bfa86246c982b4d93c2c765c95561e
pei-jung <pei-jung.lan@liferay.com>
2020-01-08 16:00:45 -0800

    LPS-103029 Add base account user screen navigation entry

---
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/frontend/taglib/servlet/taglib/BaseAccountUserScreenNavigationEntry.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.admin.web.internal.frontend.taglib.servlet.taglib;
+
+import com.liferay.account.admin.web.internal.constants.AccountScreenNavigationEntryConstants;
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.User;
+
+import java.io.IOException;
+
+import java.util.Locale;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pei-Jung Lan
+ */
+public abstract class BaseAccountUserScreenNavigationEntry
+	implements ScreenNavigationEntry<User> {
+
+	public abstract String getActionCommandName();
+
+	public abstract String getJspPath();
+
+	@Override
+	public String getLabel(Locale locale) {
+		return LanguageUtil.get(locale, getEntryKey());
+	}
+
+	@Override
+	public String getScreenNavigationKey() {
+		return AccountScreenNavigationEntryConstants.
+			SCREEN_NAVIGATION_KEY_ACCOUNT_USER;
+	}
+
+	@Override
+	public void render(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		httpServletRequest.setAttribute(
+			"ACTION_COMMAND_NAME", getActionCommandName());
+		httpServletRequest.setAttribute("EDITABLE", Boolean.TRUE);
+		httpServletRequest.setAttribute(
+			"FORM_LABEL", getLabel(httpServletRequest.getLocale()));
+		httpServletRequest.setAttribute("JSP_PATH", getJspPath());
+		httpServletRequest.setAttribute("SHOW_CONTROLS", isShowControls());
+		httpServletRequest.setAttribute("SHOW_TITLE", isShowTitle());
+
+		RequestDispatcher requestDispatcher =
+			servletContext.getRequestDispatcher("/edit_user_navigation.jsp");
+
+		try {
+			requestDispatcher.include(httpServletRequest, httpServletResponse);
+		}
+		catch (ServletException se) {
+			throw new IOException(
+				"Unable to render /edit_user_navigation.jsp", se);
+		}
+	}
+
+	protected boolean isShowControls() {
+		return true;
+	}
+
+	protected boolean isShowTitle() {
+		return true;
+	}
+
+	@Reference(target = "(osgi.web.symbolicname=com.liferay.users.admin.web)")
+	protected ServletContext servletContext;
+
+}
