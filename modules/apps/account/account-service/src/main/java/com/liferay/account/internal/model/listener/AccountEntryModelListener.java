a16898278d3f9938962572b3f7355da06e51e4c7
alee8888 <albert.lee@liferay.com>
2020-02-28 15:09:39 -0800

    LPS-106872 Reindex Account Entry after update

--- modules/apps/account/account-service/src/main/java/com/liferay/account/internal/model/listener/AccountEntryModelListener.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/internal/model/listener/AccountEntryModelListener.java
@@ -24,6 +24,9 @@ import com.liferay.account.service.AccountRoleLocalService;
 import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.search.SearchException;
 
 import java.util.List;
 
@@ -79,6 +82,23 @@ public class AccountEntryModelListener extends BaseModelListener<AccountEntry> {
 		for (AccountEntryUserRel accountEntryUserRel : accountEntryUserRels) {
 			_accountEntryUserRelLocalService.deleteAccountEntryUserRel(
 				accountEntryUserRel);
+		}
+	}
+
+	@Override
+	public void onAfterUpdate(AccountEntry accountEntry) {
+		_reindexAccountEntry(accountEntry.getAccountEntryId());
+	}
+
+	private void _reindexAccountEntry(long accountEntryId) {
+		try {
+			Indexer<AccountEntry> indexer =
+				IndexerRegistryUtil.nullSafeGetIndexer(AccountEntry.class);
+
+			indexer.reindex(AccountEntry.class.getName(), accountEntryId);
+		}
+		catch (SearchException searchException) {
+			throw new ModelListenerException(searchException);
 		}
 	}
 
