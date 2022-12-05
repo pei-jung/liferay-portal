6b72f184ac887dabb3eb258bdfab9a832be01237
pei-jung <pei-jung.lan@liferay.com>
2022-12-05 14:16:00 -0800

    LPS-169037 Regular user will have the user type

--- portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
@@ -1875,6 +1875,7 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		defaultUser.setLoginDate(date);
 		defaultUser.setFailedLoginAttempts(0);
 		defaultUser.setAgreedToTermsOfUse(true);
+		defaultUser.setType(UserConstants.TYPE_USER);
 		defaultUser.setStatus(WorkflowConstants.STATUS_APPROVED);
 
 		// Invoke updateImpl so that we do not trigger model listeners. See
