1f7dc2239cd199db120c355988605326599afd68
Gislayne Vitorino <gislayne.vitorino@liferay.com>
2023-03-27 18:43:54 -0300

    LPS-179026 Basic changes, shows the word content

--- modules/apps/wiki/wiki-web/src/main/java/com/liferay/wiki/web/internal/change/tracking/spi/display/WikiPageCTDisplayRenderer.java
+++ modules/apps/wiki/wiki-web/src/main/java/com/liferay/wiki/web/internal/change/tracking/spi/display/WikiPageCTDisplayRenderer.java
@@ -16,17 +16,28 @@ package com.liferay.wiki.web.internal.change.tracking.spi.display;
 
 import com.liferay.change.tracking.spi.display.BaseCTDisplayRenderer;
 import com.liferay.change.tracking.spi.display.CTDisplayRenderer;
+import com.liferay.change.tracking.spi.display.context.DisplayContext;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.portlet.PortletRequestModel;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.URLCodec;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.wiki.constants.WikiPortletKeys;
+import com.liferay.wiki.engine.WikiEngineRenderer;
 import com.liferay.wiki.model.WikiPage;
+import com.liferay.wiki.model.WikiPageDisplay;
+import com.liferay.wiki.service.WikiPageLocalService;
 
 import java.util.Locale;
 
 import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -38,6 +49,10 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(service = CTDisplayRenderer.class)
 public class WikiPageCTDisplayRenderer extends BaseCTDisplayRenderer<WikiPage> {
+
+	public WikiPageCTDisplayRenderer() {
+		_wikiEngineRenderer = null;
+	}
 
 	@Override
 	public String getEditURL(
@@ -72,6 +87,50 @@ public class WikiPageCTDisplayRenderer extends BaseCTDisplayRenderer<WikiPage> {
 	}
 
 	@Override
+	public String renderPreview(DisplayContext<WikiPage> displayContext)
+		throws Exception {
+
+		WikiPage wikiPage = displayContext.getModel();
+
+		HttpServletRequest httpServletRequest =
+			displayContext.getHttpServletRequest();
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		PortletRequest portletRequest =
+			(PortletRequest)httpServletRequest.getAttribute(
+				JavaConstants.JAVAX_PORTLET_REQUEST);
+		PortletResponse portletResponse =
+			(PortletResponse)httpServletRequest.getAttribute(
+				JavaConstants.JAVAX_PORTLET_RESPONSE);
+
+		PortletRequestModel portletRequestModel = new PortletRequestModel(
+			portletRequest, portletResponse);
+
+		String attachmentURLPrefix = StringBundler.concat(
+			themeDisplay.getPathMain(), "/wiki/get_page_attachment?p_l_id=",
+			themeDisplay.getPlid(), "&nodeId=", wikiPage.getNodeId(), "&title=",
+			URLCodec.encodeURL(wikiPage.getTitle()), "&fileName=");
+
+		String url = null;
+
+		String urlw = _wikiEngineRenderer.convert(
+			displayContext.getModel(), null, null, attachmentURLPrefix);
+
+		WikiPageDisplay wikiPageDisplay = _wikiPageLocalService.getPageDisplay(
+			wikiPage, null, null, attachmentURLPrefix);
+
+		return wikiPageDisplay.getFormattedContent();
+	}
+
+	@Override
+	public boolean showPreviewDiff() {
+		return true;
+	}
+
+	@Override
 	protected void buildDisplay(DisplayBuilder<WikiPage> displayBuilder) {
 		WikiPage wikiPage = displayBuilder.getModel();
 
@@ -96,6 +155,15 @@ public class WikiPageCTDisplayRenderer extends BaseCTDisplayRenderer<WikiPage> {
 	}
 
 	@Reference
+	private Language _language;
+
+	@Reference
 	private Portal _portal;
+
+	@Reference
+	private WikiEngineRenderer _wikiEngineRenderer;
+
+	@Reference
+	private WikiPageLocalService _wikiPageLocalService;
 
 }
